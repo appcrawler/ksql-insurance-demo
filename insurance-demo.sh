7cf03cb900ad391a219f23286d5100988db5a1b5
Steve Howard <stevedhoward@gmail.com>
2020-02-07 07:13:48 -0500

    Cleaned up KSQL script

--- insurance-demo.sh
+++ insurance-demo.sh
@@ -12,8 +12,7 @@ confluent local start
 #---------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------
-# create topics for orders, customers, and pizze status, as well as predefined topics
-# to hold our KSQL streams and tables
+# create topics
 
 kafka-topics --bootstrap-server localhost:9092 --create --topic customers --partitions 1 --replication-factor 1
 kafka-topics --bootstrap-server localhost:9092 --create --topic claims --partitions 1 --replication-factor 1
@@ -28,27 +27,18 @@ echo "drop table procedures;" | mysql -u root --database=test
 echo "drop table customers;" | mysql -u root --database=test
 
 #---------------------------------------------------------------------------------
-# sleep 20 seconds to ensure everything is up, then create connectors for our source
-# TIBCO queues, as well as our GCP BigQuery sink
+# sleep 20 seconds to ensure everything is up
 sleep 20
 
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @/root/claims-sink.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @/root/procedures-sink.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @/root/customers-sink.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @claims-sink.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @procedures-sink.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @customers-sink.json
 #---------------------------------------------------------------------------------
-# clean up metadata for our TIBCO producer, then run TIBCO and KSQL DDL
+#load initial events
 python load-customers-stream.py
 python load-claims-stream.py 100
 python load-providers-stream.py
 python load-procedures-stream.py
-ps -ef | grep tibemsd | grep -v grep
-if [ $? -ne 0 ]; then
-  d=$(pwd)
-  cd /tmp 
-  nohup tibemsd &
-  cd $d
-else
-  ps -ef | grep tibemsd
-fi
-echo "RUN SCRIPT '/root/anthem.ksql';" | ksql
+#run KSQL stream creations
+echo "RUN SCRIPT 'insurance-demo.ksql';" | ksql
 #---------------------------------------------------------------------------------
